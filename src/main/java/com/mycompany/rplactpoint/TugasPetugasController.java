247c2c200d916529ed53ad517e9e9bfb8cbabd31
Me <Me@Our.mshome.net>
2020-11-21 02:24:18 +0700

    Merge origin/1.0 into Ihiw
    Conflicts:
    	db_rplactpoint.db

--- src/main/java/com/mycompany/rplactpoint/TugasPetugasController.java
+++ src/main/java/com/mycompany/rplactpoint/TugasPetugasController.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.mycompany.rplactpoint;
 
 import com.mycompany.rplactpoint.databases.UserHandler;
@@ -15,7 +10,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
 import javafx.scene.text.Text;
 
-/**
+/*
  * FXML Controller class
  *
  * @author Me
